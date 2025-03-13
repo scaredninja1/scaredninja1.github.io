e2034cf979b86de6c3c1161a11aa5aabeccab9a8
scaredninja1 <scaredninja1@gmail.com>
2025-03-12 19:56:31 -0500

    adding cookie rules

--- docs/app.js
+++ docs/app.js
@@ -1,6 +1,32 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Temporary code
-    alert("Hello World");
+    
+    // Cookies
+    const now = new Date();
+    document.cookie = `now=${now}; SameSite=None; Secure`;
+    document.cookie = 'course=WebProgramming2025; SameSite=None; Secure';
+
+    document.getElementById('old-cookies').innerText = document.cookie
+
+    // Look-style cookies / Cookie Store
+    const cookieStore = window.cookieStore
+
+    cookieStore
+    .set({name: 'usernamme', value: 'scaredninja1'})
+    .then(
+        () => {console.log('Cookie set using CookieStore');}, 
+    (reason) => {
+        console.error('Unable to set cookie: ' + reason);
+    }
+    );
+    cookieStore.get('username').then(
+        (obj) => {
+            const elt = document.getElementById('new-cookies');
+            elt.innerText = `${obj.name}=${obj.value}`;
+        },
+        (reason) => { console.error('Unable to read cookie: ' + reason)}
+    );
+
+
 })
 
 
